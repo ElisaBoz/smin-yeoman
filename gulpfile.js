e2e636c7a0ae1124ffe66774ca691ee40ca0fa9a
ElisaBoz <elisa.bozon@gmail.com>
2017-12-06 10:24:47 +0100

    pour gulp page

--- gulpfile.js
+++ gulpfile.js
@@ -176,3 +176,10 @@ gulp.task('default', () => {
     runSequence(['clean', 'wiredep'], 'build', resolve);
   });
 });
+
+
+
+gulp.task('deploy', ['default'], () => {
+  return gulp.src('dist/**/*')
+    .pipe($.ghPages());
+});
